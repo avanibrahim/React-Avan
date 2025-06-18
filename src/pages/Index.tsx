eff37c5a5d275d53e6f8a17f8972b37c66511fd1
avanibrahim <138624853+avanibrahim@users.noreply.github.com>
2025-06-19 07:18:23 +0800

    update App.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,6 +8,7 @@ import Projects from '../components/Projects';
 import Certification from '../components/Certification';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
+import NotFound from './NotFound';
 
 const Index = () => {
   return (
